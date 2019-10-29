cef31614b5d05d38e8e0723def40d933a102fcea
alex <alexandre.peyrard42@gmail.com>
2019-10-29 16:22:05 +0100

    visuel des barres de ressources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { BuildingComponent } from './component/building/building.component';
 import { GamePageComponent } from './pages/game-page/game-page.component';
@@ -9,14 +8,12 @@ import { NavbarComponent } from './component/navbar/navbar.component';
 import { StockbarComponent } from './component/stockbar/stockbar.component';
 import { TravelbarComponent } from './component/travelbar/travelbar.component';
 import { TimerComponent } from './component/timer/timer.component';
-import { ControlerComponent } from './component/controler/controler.component';
-<<<<<<< HEAD
-import { ProgressbarComponent } from './component/progressbar/progressbar.component';
-=======
+import { ControlerComponent } from './component/controler/controler.component'
 import { MenuConstructBuildingComponent } from './component/menu-construct-building/menu-construct-building.component';
 import { GridComponent } from './component/grid/grid.component';
 import { CelluleComponent } from './component/cellule/cellule.component';
->>>>>>> dev
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -29,16 +26,15 @@ import { CelluleComponent } from './component/cellule/cellule.component';
     ControlerComponent,
     BuildingComponent,
     MenuConstructBuildingComponent,
-<<<<<<< HEAD
-    ProgressbarComponent
-=======
     GridComponent,
     CelluleComponent,
->>>>>>> dev
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/stockbar/stockbar.component.ts
+++ src/app/component/stockbar/stockbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-stockbar',
   templateUrl: './stockbar.component.html',
-  styleUrls: ['./stockbar.component.scss']
+  styleUrls: ['./stockbar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class StockbarComponent implements OnInit {
 
