c85af061c40bcec968d327e7f1267dd6411b5050
alex <alexandre.peyrard42@gmail.com>
2019-11-13 17:05:46 +0100

    màj au niveau du visuel des bars

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -10,18 +10,22 @@ export class GamePageComponent implements OnInit {
 
   @HostListener('window:keydown.f') cheatFood(){
     this.service.food += 100;
+    this.service.foodProgress = (this.service.food * 100) /this.service.foodMax;
   };
 
   @HostListener('window:keydown.e') cheatEnergy(){
     this.service.energy += 100;
+    this.service.energyProgress = (this.service.energy * 100) /this.service.energyMax;
   };
 
   @HostListener('window:keydown.i') cheatIron(){
     this.service.iron += 100;
+    this.service.ironProgress = (this.service.iron * 100) /this.service.ironMax;
   };
 
   @HostListener('window:keydown.h') cheatHuman(){
     this.service.human += 100;
+    this.service.humanProgress = (this.service.human * 100) /this.service.humanMax;
   }
 
   constructor(private service: GameService) { }
